f330692b5533307bcae49188653f110fbe1c7b56
kjlee2497 <kjlee2497@gmail.com>
2024-01-11 17:40:38 -0500

    Top, Side, and Bottom bars finished.  About to work on Create Post

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
