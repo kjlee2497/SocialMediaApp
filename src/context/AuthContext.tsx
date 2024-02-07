6727f8bc2e25eda72fdc2d72b25a62cb2fd3090d
Paul H Choi <paulhchoi+github@pm.me>
2024-02-07 15:55:07 -0500

    fix: correct issues with setUser typing

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,91 +1,87 @@
-import { getCurrentUser } from '@/lib/appwrite/api';
-import { IUser } from '@/types';
-import { createContext, useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { getCurrentUser } from "@/lib/appwrite/api"
+import { IUser } from "@/types"
+import { createContext, useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const INITIAL_USER = {
-    id: '',
-    name: '',
-    username: '',
-    email: '',
-    imageUrl: '',
-    bio: ''
-};
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: ""
+}
 
 const INITIAL_STATE = {
-    user: INITIAL_USER,
-    isLoading: false,
-    isAuthenticated: false,
-    setUser: () => {},
-    setIsAuthenticated: () => {},
-    checkAuthUser: async () => false as boolean,
-};
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean
+}
 
-const AuthContext = createContext(INITIAL_STATE);
+const AuthContext = createContext(INITIAL_STATE)
 
 // Every context needs children because it wraps the entire app and displays whatever is in it
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-// INITIALIZATIONS
-    const [user, setUser] = useState<IUser>(INITIAL_USER);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate = useNavigate();
-  
-    const checkAuthUser = async () => {
-        try {
-            const currentAccount = await getCurrentUser();
+  // INITIALIZATIONS
+  const [user, setUser] = useState<IUser>(INITIAL_USER)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const navigate = useNavigate()
 
-            if(currentAccount) {
-                setUser({
-                    id: currentAccount.$id,
-                    name: currentAccount.name,
-                    username: currentAccount.username,
-                    email: currentAccount.email,
-                    imageUrl: currentAccount.imageUrl,
-                    bio: currentAccount.bio
-                })
+  const checkAuthUser = async () => {
+    try {
+      const currentAccount = await getCurrentUser()
 
-                setIsAuthenticated(true);
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio
+        })
 
-                return true;
-            }
-        } catch(e) {
-            console.log(e);
-            return false;
-        } finally {
-            setIsLoading(false);
-        }
-    };
-    // We need to call the checkAuthUser function everytime we load the page so we need the below line:
-    // if localStorage does not have any cookies or if local cookies returns as null, renavigate to sign in page and then checkAuthUser()
-    useEffect(() => {
-        if(
-            localStorage.getItem('cookieFallback') === null ||
-            localStorage.getItem('cookieFallback') === '[]'
-        ) {
-            navigate('/sign-in')
-        }
+        setIsAuthenticated(true)
 
-        checkAuthUser();
-    }, []);
-    // empty array dependency makes it so that this runs everytime the app reloads
-
-    const value = {
-        user,
-        setUser,
-        isLoading,
-        isAuthenticated,
-        setIsAuthenticated,
-        checkAuthUser
+        return true
+      }
+    } catch (e) {
+      console.log(e)
+      return false
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  // We need to call the checkAuthUser function everytime we load the page so we need the below line:
+  // if localStorage does not have any cookies or if local cookies returns as null, renavigate to sign in page and then checkAuthUser()
+  useEffect(() => {
+    if (
+      localStorage.getItem("cookieFallback") === null ||
+      localStorage.getItem("cookieFallback") === "[]"
+    ) {
+      navigate("/sign-in")
     }
 
-    return (
-        <AuthContext.Provider value={value}>
-            { children }
-        </AuthContext.Provider>
-    )
+    checkAuthUser()
+  }, [navigate])
+  // empty array dependency makes it so that this runs everytime the app reloads
+
+  const value = {
+    user,
+    setUser: setUser as () => void,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated: () => {},
+    checkAuthUser: checkAuthUser as () => Promise<boolean> // Update the type here
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
 
-export default AuthProvider;
+export default AuthProvider
 
 export const useUserContext = () => useContext(AuthContext)
